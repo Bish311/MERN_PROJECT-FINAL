105ba14e67e199fb66c36a50801d272efa6145f6
Bish311 <bishwayan.24bcs10200@sst.scaler.com>
2025-10-23 13:29:24 +0530

    Basic initial stuff
    creating the initial folders and installing
     the dependencies.

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.json({ message: 'ReelVault API is running' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
